5290ff38bfc06f2c11d0ac41f8e76a18db528553
raghualamanda <raghu.alamanda@gmail.com>
2015-08-04 21:23:26 +0530

    initial

---
+++ MyHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyHome
+//
+//  Created by Anasuya Polu on 03/08/15.
+//  Copyright (c) 2015 Anasuya Polu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyHome
+//
+//  Created by Anasuya Polu on 03/08/15.
+//  Copyright (c) 2015 Anasuya Polu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
